f9b6197d9f3d843e1f289805a99cb2e14ca1adb3
Roman Yakovlev <tensegrity.666@icloud.com>
2019-09-06 15:32:01 +0300

    Добавляет цикл, завершает разработку

--- bank.js
+++ bank.js
@@ -4,4 +4,12 @@ let money = +prompt("Введите сумму вклада: ", "");
 let year = +prompt("Введите срок вклада (целое количество лет): ", "");
 let percents = +prompt("Введите желаемый процент вклада", "");
 
-for ()
+percents = percents / 100;
+let total = "Изменение размера вклада по годам:\n";
+
+for (let i = 1; i <= year; i++) {
+    money = money + (money * percents);
+    total += i + " год: " + money.toFixed(2) + " рублей;\n"
+};
+
+alert(total);
